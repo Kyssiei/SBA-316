7589052a0d7d00476ede8045a554b843cb2ab363
Kayla Gentry <kaylagentry@Kaylas-MacBook-Pro.local>
2025-02-15 20:06:21 -0500

    More structured code & updated form

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.
+// moving banner
 const strip = document.querySelector(".moving-strip p");
 
 const textWidth = strip.offsetWidth;
@@ -6,3 +6,26 @@ const screenWidth = window.innerWidth;
 const duration = (screenWidth + textWidth) / 100;
 
 strip.style.animation = `moveText ${duration}s linear infinate`;
+
+// "Join the club" Form
+const button = document.getElementById("joinButton")
+const par = document.getElementById("par")
+const title = document.getElementById("con2title")
+const formPopup = document.getElementById("formPopup")
+
+
+console.log(button);
+console.log(par);
+console.log(title);
+console.log(formPopup);
+
+
+
+button.onclick = function () {
+    con2title.textContent = "Hey Girl!";
+    par.textContent = "Please fill out the form below and press submit.";
+    formPopup.style.display = 'block';
+    button.style.display = 'none'
+
+
+}
